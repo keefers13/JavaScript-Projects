6e49953f7882fbba4d0ffea9c029d5eea2834921
keefers13 <keithmoore13@gmail.com>
2022-03-24 16:51:50 -0600

    pushing to main

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+document.write("\"Hello, World!\"" //displays the text "Hello, World!"
++ "- Every Programmer Ever<br>");
+var A = "Hola"; 
+window.alert(A); //alert that says Hola
+var B = "Hello," + " World!" // assigning value to var
+document.write(B); //writing var B
+var Keith = "Me", Dad = "Rob", Mom = "Carrie" //assigning multiple variables
+document.write(Keith); //writing var
+document.write(Mom); //writing var
+document.write(3 + 3); //displaying sum of 3 + 3
+document.write(Keith.fontcolor( "blue" ));
+document.write("<br>"
++ Keith);
