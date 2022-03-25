3e01012fb5e3a1ad681a9a799b18368101a297e3
keefers13 <keithmoore13@gmail.com>
2022-03-24 18:20:21 -0600

    pushing to main

---
+++ Basic JavaScript Projects/Project2_funcitons/JS/main.js
@@ -0,0 +1,5 @@
+function first_function() { //named the function
+    var X = "Value of Variable"; //stated the variable
+    X += " is shown here."; //concatenated the variable value
+    document.getElementById("html-element").innerHTML = X; //defined the event
+}
