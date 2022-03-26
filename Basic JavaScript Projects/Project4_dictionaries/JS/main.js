f2fcb8a424af242a68ad4f185b993ea929676c90
keefers13 <keithmoore13@gmail.com>
2022-03-25 21:05:36 -0600

    updated

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,10 +1,10 @@
-function my_Dictionary() {
+function my_Dictionary() {   //created a dicitonary
     var Player = {
         First:"Nikola",
         Last:"Jokic",
         JerseyNumber:15,
         Team:"Denver",
     };
-    delete Player.Team;
+    delete Player.Team; //deleted the item I wanted to be in the output
     document.getElementById("Dictionary").innerHTML = Player.Team;
 }
