a1f61527a41a191d5e3061207c5448474946b038
keefers13 <keithmoore13@gmail.com>
2022-03-26 15:24:17 -0600

    pushing to main

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,54 @@
+document.write(typeof 5.3); //testing new javascript knowledge
+document.write("<br>");
+document.write("10" + 5); //testing new javascript knowledge
+document.write("<br>");
+document.write(0/0); //testing new javascript knowledge
+document.write("<br>");
+document.write(isNaN(10)); //testing new javascript knowledge
+document.write("<br>");
+document.write(isNaN("True")); //testing new javascript knowledge
+document.write("<br>");
+document.write(2E310); //testing new javascript knowledge
+document.write("<br>");
+document.write(-2E310); //testing new javascript knowledge
+document.write("<br>");
+function test_function() { //testing new javascript knowledge
+    var X = 2E310;
+document.getElementById("Test1").innerHTML = X;
+}
+function testing_function() { //testing new javascript knowledge
+    var Y = -2E310;
+document.getElementById("Test2").innerHTML = Y;
+}
+document.write("<br>");
+document.write(10 > 9); //testing new javascript knowledge
+document.write("<br>");
+document.write(10 < 9); //testing new javascript knowledge
+document.write("<br>");
+console.log(2 + 2); //testing new javascript knowledge
+document.write("<br>");
+console.log(10 < 1); //testing new javascript knowledge
+document.write("<br>");
+document.write(10 == 10); //testing new javascript knowledge
+document.write("<br>");
+document.write(10 == 11); //testing new javascript knowledge
+document.write("<br>");
+document.write(11 === 11); //testing new javascript knowledge
+document.write("<br>");
+document.write(11 === "hi"); //testing new javascript knowledge
+document.write("<br>");
+document.write(10 === "10"); //testing new javascript knowledge
+document.write("<br>");
+document.write(10 === 11); //testing new javascript knowledge
+document.write("<br>");
+document.write(10 > 5 && 2 > 1); //testing new javascript knowledge
+document.write("<br>");
+document.write(10 > 5 && 3 > 4); //testing new javascript knowledge
+document.write("<br>");
+document.write(10 > 5 || 3 > 4); //testing new javascript knowledge
+document.write("<br>");
+document.write(10 < 5 || 3 > 4); //testing new javascript knowledge
+document.write("<br>");
+document.write(!(20 > 10)); //testing new javascript knowledge
+document.write("<br>");
+document.write(!(5 > 10)); //testing new javascript knowledge
